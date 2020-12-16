672353e3c867811bf9665be23a24270dd64e9b4b
Dev-wonderful <adeonederful20@gmail.com>
2020-12-16 15:35:45 +0100

    commit on 'master'

--- src/Checkout system/App.js
+++ src/Checkout system/App.js
@@ -16,7 +16,9 @@ class App extends Component {
       {"id":18,"title":"MBJ Women's Solid Short Sleeve Boat Neck V ","price":9.85,"description":"95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem","category":"women clothing","image":"https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg"},{"id":19,"title":"Opna Women's Short Sleeve Moisture","price":7.95,"description":"100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort","category":"women clothing","image":"https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg"},{"id":20,"title":"DANVOUY Womens T Shirt Casual Cotton Short","price":12.99,"description":"95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.","category":"women clothing","image":"https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"}
     ],
 
-    sideBar : false
+    sideBar : false ,
+
+    quantity : []
    }
 
    
--- src/Checkout system/products.jsx
+++ src/Checkout system/products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
-import './App.css';
+import './product.css';
+//import '../index.css';
 //import Displayproducts from './Displayproducts';
 
 const Products = ({ product, onAddToCart }) => {
@@ -18,12 +19,12 @@ const Products = ({ product, onAddToCart }) => {
             return(
                 <>
                     
-                    <div className='products'>
+                    <div className='products py-5 flex-initial'>
 
-                        <div className='image-container'>
+                        <div className='image-container box-border'>
 
 
-                            <img id='image' src={item.image} alt={item.title}/>
+                            <img className=' w-full h-full' src={item.image} alt={item.title}/>
 
                             
 
@@ -31,8 +32,19 @@ const Products = ({ product, onAddToCart }) => {
 
                         <div className='item-container'>
                             
-                            <div className='title'><p>{item.title}</p></div>
-                            <div className='price'><p>${item.price}</p><button className='btn' onClick={() => props.onAddToCart(item)}>Add To Cart</button></div>
+                            <div className='title px-3 py-3 leading-none overflow-y-auto'><p>{item.title}</p></div>
+                           
+                            <div className='price space-x-2 leading-none px-2'>
+                                <p>${item.price}</p>
+
+                                <div className='qty flex flex-row px-2'>
+                                    <button className='decrement flex-1'>-</button>
+                                    <span className='qty-display flex-1'>1</span>
+                                    <button className='increment flex-1'>+</button>
+                                </div>
+
+                                <button className='btn w-1/3' onClick={() => props.onAddToCart(item)}>Add To Cart</button>
+                            </div>
                             
                         </div>
 
@@ -47,7 +59,7 @@ const Products = ({ product, onAddToCart }) => {
 
 
         return(
-            <div className='container container-flex '>
+            <div className='container container-flex h-screen w-screen '>
                 {product.map(items => <Displayproducts key={items.id} products={items} onAddToCart={onAddToCart}/>)}
             </div>
         )
