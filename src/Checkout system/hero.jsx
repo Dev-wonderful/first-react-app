7e30050c3f3bbd018cead60399d576c97a5cd679
Dev-wonderful <adeonederful20@gmail.com>
2021-01-25 15:34:36 +0100

    commit on 'master'

--- src/Checkout system/hero.jsx
+++ src/Checkout system/hero.jsx
@@ -4,8 +4,16 @@ const Hero = () => {
     return ( 
         <div className="hero">
             <div className="hero-container">
-                <h1>Journey To Commercial Redemption</h1>
+                <h1>Journey To Commercial Comfort</h1>
                 <p>Shop with ease</p>
+            </div>
+
+            <div className='action'>
+                <button>All Categories</button>
+                <button>Electronics</button>
+                <button>Jewelery</button>
+                <button>Women Clothing</button>
+                <button>Men Clothing</button>
             </div>
         </div>
      );
